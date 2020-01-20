72be2c36e1af65308d851b188997b20120d4337d
milad_jalali <miladsj1994@gmail.com>
2020-01-20 12:53:11 +0330

    add bs4 imports

--- namava.py
+++ namava.py
@@ -1,6 +1,7 @@
 from decorators import exception_logger
+from bs4 import BeautifulSoup
 
 
 @exception_logger
-def parse_page(html_obj):
-    pass
+def parse_page(html_text):
+    html_obj = BeautifulSoup(html_text)
