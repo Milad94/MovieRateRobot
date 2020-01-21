e48e0a3de2914415abfb2d67bfded106ce1d1a56
milad_jalali <miladsj1994@gmail.com>
2020-01-21 19:48:35 +0330

    secure config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from peewee import MySQLDatabase
-from local_config import USERNAME, PASSWORD, DATABASE_NAME
+from local_config import USERNAME, PASSWORD, DATABASE_NAME,HOST,PORT
 
 db = MySQLDatabase(database=DATABASE_NAME, user=USERNAME,
-                   passwd=PASSWORD, host='localhost',
-                   port=3306)
+                   passwd=PASSWORD, host=HOST,
+                   port=PORT)
