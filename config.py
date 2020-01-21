b44b3cabf3ee822223e53d7e41c80ce3d67396f1
milad_jalali <miladsj1994@gmail.com>
2020-01-21 19:45:14 +0330

    correct config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from peewee import MySQLDatabase
-from local_config import USERNAME, PASSWORD
+from local_config import USERNAME, PASSWORD, DATABASE_NAME
 
-db = MySQLDatabase(database="MovieRateRobot", user=USERNAME,
+db = MySQLDatabase(database=DATABASE_NAME, user=USERNAME,
                    passwd=PASSWORD, host='localhost',
                    port=3306)
