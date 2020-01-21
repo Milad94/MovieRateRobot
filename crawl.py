dbaa2e8d7adcb093250ba93a208926a4dddda96f
milad_jalali <miladsj1994@gmail.com>
2020-01-21 13:45:24 +0330

    use grequests to send  parallel requests

--- crawl.py
+++ crawl.py
@@ -1,3 +1,7 @@
+import time
+
+import grequests
+
 from request import send_get_request
 from decorators import exception_logger
 import filimo
@@ -10,18 +14,20 @@ import json
 def _crawl_namva_pages():
     url = "https://www.namava.ir/api/v1.0/medias/latest-movies?pi={}&ps=30"
     crawl = True
-    page_number = 1
+    page_number = 100
     pages = list()
     while crawl:
-        response = send_get_request(url.format(page_number))
-        page_number += 1
-        if response.ok:
-            page = json.loads(response.text)
-            if page['result']:
-                pages.append(namava.parse_page(page))
-            else:
-                crawl = False
-
+        urls = [url.format(i) for i in range(1, page_number)]
+        rs = (grequests.get(u) for u in urls)
+        responses = grequests.map(rs)
+        page_number += 100
+        for response in responses:
+            if response.ok:
+                page = json.loads(response.text)
+                if page['result']:
+                    pages.append(namava.parse_page(page))
+                else:
+                    crawl = False
     return pages
 
 
@@ -30,12 +36,15 @@ def _crawl_namava_movies(pages):
     movies_data = list()
     movie_url = "https://www.namava.ir/api/v1.0/medias/{}/preview"
     for page in pages:
+        movie_urls = list()
         for movie_id in page:
-            response = send_get_request(movie_url.format(movie_id))
+            movie_urls.append(movie_url.format(movie_id))
+        rs = (grequests.get(u) for u in movie_urls)
+        responses = grequests.map(rs)
+        for response in responses:
             if response.ok:
                 movie_json = json.loads(response.text)
                 movies_data.append(namava.pars_movie(movie_json))
-
     return movies_data
 
 
