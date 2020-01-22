c9adabcd0f3431a5ec50de743e6c5a049411737f
milad_jalali <miladsj1994@gmail.com>
2020-01-23 01:32:57 +0330

    refactore database handler with factory method pattern

--- database.py
+++ database.py
@@ -21,18 +21,34 @@ class DatabaseHandler(metaclass=Sinleton):
         return movie_instance
 
     @exception_logger
-    def insert_movies(self, movies, site):
-        for movie in movies:
-            if bool(self.__check_movie_exist(movie)):
-                instance = eval(site).create(rate=movie['rate'],
-                                             movie=self.__check_movie_exist(
-                                                 movie))
-            else:
-                movie_instance = self.__save_movie(movie)
-                instance = eval(site).create(rate=movie['rate'],
-                                             movie=movie_instance)
-            return instance.save()
+    def __insert_if_not_exist(self, movie, site):
+        movie_instance = self.__save_movie(movie)
+        return eval(site).create(rate=movie['rate'],
+                                 movie=movie_instance)
 
     @exception_logger
-    def __check_movie_exist(self, movie):
-        return Movie.get(Movie.caption == movie['caption'])
+    def __insert_if_exist(self, movie, site):
+        return eval(site).create(rate=movie['rate'],
+                                 movie=self.get_movie_from_db_if_exist(
+                                     movie))
+
+    @exception_logger
+    def insert_movies(self, movies, site):
+        for movie in movies:
+            instance = self.save_movie_in_db(
+                self.get_movie_from_db_if_exist(movie))
+            instance(movie, site).save()
+
+    @exception_logger
+    def get_movie_from_db_if_exist(self, movie):
+        try:
+            record = Movie.get(Movie.caption == movie['caption'])
+        except:
+            return False
+        return record
+
+    @exception_logger
+    def save_movie_in_db(self, is_exist):
+        if bool(is_exist):
+            return self.__insert_if_exist
+        return self.__insert_if_not_exist
